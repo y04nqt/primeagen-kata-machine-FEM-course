77b0578c4296ff888178232c2c5f118e421d1475
nnnnQT <cortana@s007xcpu2552.lan>
2023-04-18 10:30:14 -0500

    implemented bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,11 @@
 export default function bubble_sort(arr: number[]): void {
-
+    for(let i = 0; i < arr.length; ++i) {
+        for (let j = 0; j < arr.length - 1 - i; ++j) {
+            if (arr[j] > arr[j+1]) {
+                const tmp = arr[j]; // store first item for switch
+                arr[j] = arr[j+1]; // set second item to first position
+                arr[j+1] = tmp; // set first item to second position
+            }
+        }
+    }
 }
